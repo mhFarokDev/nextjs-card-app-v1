e15cf5e53702cfdf10cdbf8ec1c6584121326e43
mhFarokDev <developerfaruk11@gmail.com>
2023-11-22 00:44:48 +0600

    change local API to deploy API

--- libs/GetAllusers.js
+++ libs/GetAllusers.js
@@ -3,14 +3,14 @@ import axios from "axios";
 // get all users
 export const getAllUserData = async () =>{
     // axios
-    const response = await axios.get("http://localhost:3000/api/users")
+    const response = await axios.get("https://nextjs-card-app-v1-rhx8awfnq-faruks-projects-36caa9d9.vercel.app/api/users")
     if (!response.data) throw new Error("User data not found!")
     return response.data;
 }
 
 
 export const createNewUser = async (data) =>{
-    const response = await axios.post("http://localhost:3000/api/users", data)
+    const response = await axios.post("https://nextjs-card-app-v1-rhx8awfnq-faruks-projects-36caa9d9.vercel.app/api/users", data)
     if (!response.data) {
         throw new Error("userdata not created!")
     }
